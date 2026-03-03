c31363aa575d758b71685ff2d666ba41f46acd52
ІМ-24 Владислав Коваленко <kovalenkov222@gmail.com>
2026-03-03 13:46:26 +0000

    feat: add empty methods to class

---
+++ MapView/fileReader.py
@@ -0,0 +1,17 @@
+from csv import reader
+import config
+
+
+class FileReader:
+    def __init__(
+        self, data_filename: str,
+    ) -> None:
+        pass
+
+    def read(self):
+        return [1, 2, 3]
+
+    def startReading(self, *args, **kwargs):
+
+    def stopReading(self, *args, **kwargs):
+
