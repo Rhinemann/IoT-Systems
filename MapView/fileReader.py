a899ef6a6e4228e49a079f423c33f353e89cb677
ІМ-24 Владислав Коваленко <kovalenkov222@gmail.com>
2026-03-03 14:10:18 +0000

    feat: get indexes of file header fields

--- MapView/fileReader.py
+++ MapView/fileReader.py
@@ -16,6 +16,10 @@ class FileReader:
         self.file = open(self.file_path, newline='')
         self.file_reader = reader(self.file)
         file_header = next(self.file_reader)
+        
+        self.x_idx = file_header.index('X')
+        self.y_idx = file_header.index('Y')
+        self.z_idx = file_header.index('Z')
 
     def stopReading(self, *args, **kwargs):
         if self.file:
