92c20ef6121b3d67967505470a8b67fbb016c48a
ІМ-24 Владислав Коваленко <kovalenkov222@gmail.com>
2026-03-03 13:59:40 +0000

    feat: open file

--- MapView/fileReader.py
+++ MapView/fileReader.py
@@ -6,12 +6,17 @@ class FileReader:
     def __init__(
         self, data_filename: str,
     ) -> None:
+        self.file_path = data_filename
         pass
 
     def read(self):
         return [1, 2, 3]
 
     def startReading(self, *args, **kwargs):
+        self.file = open(self.file_path, newline='')
+        self.file_reader = reader(self.acc_file)
+        file_header = next(self.file_reader)
 
     def stopReading(self, *args, **kwargs):
+        pass
 
