b10aec1020d402dbf7822aa6873aac34c2335f3b
esk4nz <grinko27052005@gmail.com>
2026-03-23 22:57:15 +0200

    SCRUM-80 Changed print to logging in agent main

--- agent/src/main.py
+++ agent/src/main.py
@@ -1,6 +1,7 @@
 from paho.mqtt import client as mqtt_client
 from schema.aggregated_data_schema import AggregatedDataSchema
 from file_datasource import FileDatasource
+import logging
 import config
 
 
@@ -28,7 +29,7 @@ def publish(client, topic, datasource):
         data = datasource.read()
         msg = AggregatedDataSchema().dumps(data)
         result = client.publish(topic, msg)
-        print(f"Published to {topic}: {msg[:50]}...")
+        logging.info(f"Published to {topic}: {msg[:50]}...")
         status = result[0]
         if status != 0:
             print(f"Failed to send message to topic {topic}")
